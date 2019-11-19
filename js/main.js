6dde058b35fb97623389759348a6de906b067692
Davydkin Valery <st.valeriq@gmail.com>
2019-11-19 15:08:37 +0700

    footer rest

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.objects-slider').slick({
+        nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+        prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+        infinite: true,
+        dots: true,
+    });
+
+  });
